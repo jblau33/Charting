acd3a399b39c91a088f4dccf341f624473ffedc3
jblau <jblau@SBA1-00869>
2022-01-04 15:43:58 -0800

    Incereased shart resolution

--- charting.py
+++ charting.py
@@ -1,2 +1,26 @@
-print("This is a charting program")
-print("This is a change to the program")
+from tkinter import *
+#from PIL import ImageTK, Image
+from PIL import *
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+root = Tk()
+root.title = ("TKinter Charting Exercise")
+root.iconbitmap ("C:\pythonprojects\charting\camera.ico")
+root.geometry("400x200")
+
+
+def graph():
+    house_prices = np.random.normal(200000,25000, 5000)
+    plt.hist(house_prices, 500)
+    plt.show()
+
+my_button = Button(root, text = "graph it!", command = graph)
+my_button.pack()
+root.mainloop()
+
+
+
+
+
